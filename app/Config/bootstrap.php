6c0dbad5409491e79f0e7b542fc48f032d073749
kasper <barathrumm@gmail.com>
2013-06-23 20:34:23 +0200

    added Crud plugin submodule by Jippi

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -72,6 +72,7 @@ Cache::config('default', array('engine' => 'File'));
  *
  */
 CakePlugin::load('DebugKit', array());
+CakePlugin::load('Crud', array());
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By Default CakePHP bundles two filters:
--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,35 +1,63 @@
 <?php
 /**
- * Application level Controller
+ * Application wide controller
  *
- * This file is application-wide controller file. You can put all
- * application-wide controller-related methods here.
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @abstract
+ * @package App.Controller
  */
-App::uses('Controller', 'Controller');
+abstract class AppController extends Controller {
 
 /**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @package		app.Controller
- * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ * List of components which can handle action invocation
+ * @var array
  */
-class AppController extends Controller {
+	public $dispatchComponents = array();
+
+/**
+ * Dispatches the controller action. Checks that the action exists and isn't private.
+ *
+ * If Cake raises MissingActionException we attempt to execute Crud
+ *
+ * @param CakeRequest $request
+ * @return mixed The resulting response.
+ * @throws PrivateActionException When actions are not public or prefixed by _
+ * @throws MissingActionException When actions are not defined and scaffolding and CRUD is not enabled.
+ */
+	public function invokeAction(CakeRequest $request) {
+		try {
+			return parent::invokeAction($request);
+		} catch (MissingActionException $e) {
+			// Check for any dispatch components
+			if (!empty($this->dispatchComponents)) {
+				// Iterate dispatchComponents
+				foreach ($this->dispatchComponents as $component => $enabled) {
+					// Skip them if they aren't enabled
+					if (empty($enabled)) {
+						continue;
+					}
+
+					// Skip if isActionMapped isn't defined in the Component
+					if (!method_exists($this->{$component}, 'isActionMapped')) {
+						continue;
+					}
+
+					// Skip if the action isn't mapped
+					if (!$this->{$component}->isActionMapped($request->params['action'])) {
+						continue;
+					}
+
+					// Skip if executeAction isn't defined in the Component
+					if (!method_exists($this->{$component}, 'executeAction')) {
+						continue;
+					}
+
+					// Execute the callback, can return CakeResponse object
+					return $this->{$component}->executeAction();
+				}
+			}
+
+			// No additional callbacks, re-throw the normal Cake exception
+			throw $e;
+		}
+	}
 }
