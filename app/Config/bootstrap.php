7543434199ee10da62cfa30793084382b6464386
kasper jensen <sebb@sebb.dk>
2014-03-19 10:52:20 +0100

    include std routes and bootstraps

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -73,7 +73,7 @@ Cache::config('default', array('engine' => 'File'));
  */
 CakePlugin::load('DebugKit', array());
 CakePlugin::load('Crud', array());
-CakePlugin::load('Assets', array());
+CakePlugin::load('Assets', array( 'routes' => true, 'bootstrap' => true));
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By Default CakePHP bundles two filters:
